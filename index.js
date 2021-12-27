ad8d52a8b9b0fb817134ae6709698fbd6d1de16e
Erik Egliens <erik@bellcom.ee>
2021-12-27 21:33:15 +0200

    added express server module and nodemon

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-const fs = require('fs')
-
-const data = `
-123454654654654asdfsdfsdcvxcv
-` 
-
-fs.writeFileSync('nodejs.txt', data)
-
-const result = fs.readFileSync('nodejs.txt', {encoding: 'utf-8'})
-
-console.log(result);
-
-console.log(__dirname);
-console.log(__filename);
+const express = require('express')
