3876859e65a002fbb0974232dddaa1b371f3fc1d
Erik Egliens <erik@bellcom.ee>
2021-12-27 21:47:33 +0200

    added css and ejs file

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 
-
 const app = express()
 
-app.get('/', (request, result) => {
-    result.end('123')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 app.listen(3000, () => {
-    console.log('app server has started on port 3000 ...');
+  console.log('Server has started on port 3000...')
 })
