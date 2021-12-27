955784ed7c69d6dcf440ff4663ca942c7b818732
Erik Egliens <erik@bellcom.ee>
2021-12-27 21:43:03 +0200

    some basic commands in node and nodemon

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const express = require('express')
+
+
+const app = express()
+
+app.get('/', (request, result) => {
+    result.end('123')
+})
+
+app.listen(3000, () => {
+    console.log('app server has started on port 3000 ...');
+})
