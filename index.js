6f88d2cbb6fce27556786eff7ac683f631c77b16
Erik Egliens <erik@bellcom.ee>
2021-12-27 23:29:30 +0200

    <p class=error><%=error%></p>

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const weatherRequest = require('./requests/weather.request')
+const express = require("express");
+const bodyParser = require("body-parser");
+const weatherRequest = require("./requests/weather.request");
 
-const app = express()
-//key
-//47697a9c980d7c51ac12a9113f785f11
+const app = express();
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index", {weather: null, error: null});
+});
 
-app.post('/', (req, res) => {
-  const { city } = req.body
+app.post("/", async (req, res) => {
+  const { city } = req.body;
 
-  weatherRequest(city)
-  res.render('index')
-})
+  const {weather, error} = await weatherRequest(city);
+  res.render("index", {weather, error} );
+});
 
 app.listen(3000, () => {
-  console.log('Server has started on port 3000...')
-})
+  console.log("Server has started on port 3000...");
+});
